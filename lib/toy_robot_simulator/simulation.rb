cd24c0d94f43b5691f68e0cc170ec5636cd7913f
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-11 13:06:44 -0300

    Add support for user input and feedback

--- lib/toy_robot_simulator/simulation.rb
+++ lib/toy_robot_simulator/simulation.rb
@@ -6,7 +6,12 @@ module ToyRobotSimulator
     end
 
     def start
-      @output.puts "Welcome to Toy Robot Simulator!"
+      @output.puts "Welcome to Toy Robot Simulator! (Press Ctrl+D to exit.)"
+    end
+
+    def input(command)
+      @output.print "#{command.chomp}..."
+      @output.print " done\n"
     end
   end
 end
--- spec/lib/toy_robot_simulator/simulation_spec.rb
+++ spec/lib/toy_robot_simulator/simulation_spec.rb
@@ -10,11 +10,25 @@ module ToyRobotSimulator
       expect(simulation).to respond_to :start
     end
 
+    it 'accepts user input', public: true do
+      expect(simulation).to respond_to :input
+    end
+
     context 'when started' do
 
       it 'outputs a welcome message', public: true do
-        expect(output).to receive(:puts).with('Welcome to Toy Robot Simulator!')
+        expect(output).to receive(:puts).with('Welcome to Toy Robot Simulator! (Press Ctrl+D to exit.)')
         simulation.start()
+      end
+    end
+
+    context 'on user input', public: true do
+
+      it 'gives immediate feedback' do
+        command = 'PLACE 0,0,SOUTH'
+        allow(output).to receive(:print) # let's ignore other calls to :print
+        expect(output).to receive(:print).with command + '...'
+        simulation.input command
       end
     end
   end
