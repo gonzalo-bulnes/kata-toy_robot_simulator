073a9956cc5989bb35472cbd53376d3c7bdc2c95
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-11 16:03:01 -0300

    Add MOVE command handling

--- lib/toy_robot_simulator/robot.rb
+++ lib/toy_robot_simulator/robot.rb
@@ -12,6 +12,20 @@ module ToyRobotSimulator
       @location = [0, 1, :north]
     end
 
+    # Update the robot current location one unit toward in the direction it faces
+    def move
+      case location.last
+      when :north
+        @location[1] += 1
+      when :east
+        @location[0] += 1
+      when :south
+        @location[1] -= 1
+      when :west
+        @location[0] -= 1
+      end
+    end
+
     # Update the robot current location
     def place(location)
       @location = location
--- spec/lib/toy_robot_simulator/robot_spec.rb
+++ spec/lib/toy_robot_simulator/robot_spec.rb
@@ -9,8 +9,12 @@ module ToyRobotSimulator
       expect(robot).to respond_to :report
     end
 
-    it 'can be placed ont the table', public: true do
+    it 'can be placed on the table', public: true do
       expect(robot).to respond_to :place
+    end
+
+    it 'can move', public: true do
+      expect(robot).to respond_to :move
     end
 
     it "by default is located at '0,1,NORTH'", private: true do
@@ -28,6 +32,41 @@ module ToyRobotSimulator
 
       it 'updates the robot current location' do
         expect{ robot.place([3, 5, :west]) }.to change{ robot.report }.from('0,1,NORTH').to('3,5,WEST')
+      end
+    end
+
+    describe '#move', public: true do
+
+      context 'when the robot is facing EAST' do
+        it 'moves the robot 1 unit toward EAST (first corrdinate increase)' do
+          robot.place([3, 2, :east])
+
+          expect{ robot.move }.to change{ robot.report }.from('3,2,EAST').to('4,2,EAST')
+        end
+      end
+
+      context 'when the robot is facing SOUTH' do
+        it 'moves the robot 1 unit toward SOUTH (second coordinate descrease)' do
+          robot.place([3, 2, :south])
+
+          expect{ robot.move }.to change{ robot.report }.from('3,2,SOUTH').to('3,1,SOUTH')
+        end
+      end
+
+      context 'when the robot is facing WEST' do
+        it 'moves the robot 1 unit toward WEST (first coordinate decrease)' do
+          robot.place([3, 2, :west])
+
+          expect{ robot.move }.to change{ robot.report }.from('3,2,WEST').to('2,2,WEST')
+        end
+      end
+
+      context 'when the robot is facing NORTH' do
+        it 'moves the robot 1 unit toward NORTH (second coordinate increase)' do
+          robot.place([3, 2, :north])
+
+          expect{ robot.move }.to change{ robot.report }.from('3,2,NORTH').to('3,3,NORTH')
+        end
       end
     end
   end
