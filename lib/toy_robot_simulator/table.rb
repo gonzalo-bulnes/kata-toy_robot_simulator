e0545e478637a680bec84e8378de19a0c0d5ae04
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-11 21:43:00 -0300

    Add failing specs for table boundaries constraints on moves
    Tables won't be infinite anymore.

--- lib/toy_robot_simulator/table.rb
+++ lib/toy_robot_simulator/table.rb
@@ -2,5 +2,16 @@ module ToyRobotSimulator
 
   # A table
   class Table
+
+    # Determine if a location is off the table boundaries
+    #
+    # For now, the table is infinite :)
+    #
+    # location - an [x, y] Array of two numbers
+    #
+    # Returns false.
+    def beyond_boundaries?(location)
+      false
+    end
   end
 end
--- spec/lib/toy_robot_simulator/table_spec.rb
+++ spec/lib/toy_robot_simulator/table_spec.rb
@@ -5,5 +5,8 @@ module ToyRobotSimulator
 
     let(:table) { Table.new() }
 
+    it 'can determine if a position is beyond its boundaries', public: true do
+      expect(table).to respond_to :beyond_boundaries?
+    end
   end
 end
